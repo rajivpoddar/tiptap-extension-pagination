9816b19d2a86015dd5920f54d5ce085c9a134762
Hugo Burton <burto732@gmail.com>
2024-12-24 18:27:57 +1000

    Include all @tiptap imports as external

--- rollup.config.js
+++ rollup.config.js
@@ -35,9 +35,8 @@ const config = {
             extensions: [".ts"],
             exclude: "node_modules/**",
         }),
-        commonjs(),
-        typescript(),
     ],
+    external: [/^@tiptap\//],
 };
 
 module.exports = config;
