eaf5eb6f1094815abd1a2d218575e2f56ed6d738
Hugo Burton <burto732@gmail.com>
2024-12-24 18:24:35 +1000

    Specify babel bundled

--- rollup.config.js
+++ rollup.config.js
@@ -7,28 +7,32 @@ const babel = require("@rollup/plugin-babel");
 const typescript = require("rollup-plugin-typescript2");
 
 const config = {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.js",
-      format: "esm",
-      exports: "named",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    autoExternal({ packagePath: "./package.json" }),
-    sourcemaps(),
-    babel(),
-    commonjs(),
-    typescript(),
-  ],
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/index.cjs.js",
+            format: "cjs",
+            exports: "named",
+            sourcemap: true,
+        },
+        {
+            file: "dist/index.js",
+            format: "esm",
+            exports: "named",
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        autoExternal({ packagePath: "./package.json" }),
+        sourcemaps(),
+        babel({
+            babelHelpers: "bundled",
+            extensions: [".ts"],
+            exclude: "node_modules/**",
+        }),
+        commonjs(),
+        typescript(),
+    ],
 };
 
 module.exports = config;
