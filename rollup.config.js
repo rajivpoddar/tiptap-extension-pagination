cec7c6bb41c5ea5bc5b7b8f5c54d5814e349764b
Hugo Burton <burto732@gmail.com>
2025-02-02 14:09:54 +1000

    Update rollup and build

--- rollup.config.js
+++ rollup.config.js
@@ -4,34 +4,32 @@
  * @description Rollup configuration file
  */
 
+import typescript from "@rollup/plugin-typescript";
 import autoExternal from "rollup-plugin-auto-external";
-import sourcemaps from "rollup-plugin-sourcemaps";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
-import typescript from "rollup-plugin-typescript2";
 
 const config = {
     input: "src/index.ts",
     output: [
         {
-            file: "dist/index.cjs.js",
+            file: "dist/index.cjs",
             format: "cjs",
             exports: "named",
+            sourcemap: true,
         },
         {
             file: "dist/index.js",
             format: "esm",
             exports: "named",
+            sourcemap: true,
         },
     ],
     plugins: [
         autoExternal({ packagePath: "./package.json" }),
-        sourcemaps(),
         commonjs(),
         typescript({
             tsconfig: "./tsconfig.json",
-            useTsconfigDeclarationDir: true,
-            declaration: true,
         }),
         babel({
             babelHelpers: "bundled",
