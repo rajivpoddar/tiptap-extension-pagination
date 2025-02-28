c3d4a6a369f1693580617847fa36fbf286342cbb
Hugo Burton <burto732@gmail.com>
2025-03-01 09:03:05 +1000

    Add undefinable helper type

--- src/types/record.ts
+++ src/types/record.ts
@@ -10,3 +10,10 @@
  * @returns The record or null.
  */
 export type Nullable<T> = T | null;
+
+/**
+ * A type definition for an undefined record. Allows records to be either T or undefined.
+ * @param T - The type of the record.
+ * @returns The record or undefined.
+ */
+export type Undefinable<T> = T | undefined;
